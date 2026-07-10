5bf90e898d843fdb048d2bbbf6eb1153a380a801
avalon1412 <dikiarch@gmail.com>
2026-07-10 23:13:01 +0700

    fix hmr by use Vite Instance

--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
     rolldownOptions: {
       input: "resources/js/app.ts",
       output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
       },
     },
   },
